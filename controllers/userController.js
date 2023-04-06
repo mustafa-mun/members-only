dba8685387e17cf33f200467ee5265a80c329fab
Mustafa <mustafamcm9@gmail.com>
2023-04-06 21:37:22 +0300

    become admin request   This commit adds become admin get and post requests

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,12 +39,51 @@ exports.post_become_member = [
 ];
 
 exports.get_become_admin = (req, res, next) => {
-  res.send("become admin get");
+  res.render("admin", { title: "Enter admin passcode", user: req.user });
 };
 
-exports.post_become_admin = (req, res, next) => {
-  res.send("become admin post");
-};
+exports.post_become_admin = [
+  body("password", "Admin passcode is not correct!").equals(
+    process.env.ADMIN_PASSCODE
+  ),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors, render the page again with errors
+      res.render("admin", {
+        title: "Enter admin passcode",
+        user: req.user,
+        errors: errors.array(),
+      });
+    } else {
+      // Passcode is correct, check if user is a member
+      if (req.user.is_member) {
+        // User is member, make user admin and redirect
+        async function updateField() {
+          try {
+            await User.findByIdAndUpdate(
+              req.user._id,
+              { is_admin: true },
+              { new: true }
+            );
+          } catch (error) {
+            return next(error);
+          }
+        }
+        updateField().then(() => res.redirect("/"));
+      } else {
+        // User is not a member, render the becoming member page again
+        const errors = ["You are not a member you can't be an admin!"];
+        res.render("member", {
+          title: "Become member first!",
+          user: req.user,
+          errors: errors,
+        });
+      }
+    }
+  },
+];
 
 exports.get_create_message = (req, res, next) => {
   res.send("become create message get");
