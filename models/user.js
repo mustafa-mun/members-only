8d2d9e7d5f8fd657310312b65b88bfe76566b0e1
Mustafa <mustafamcm9@gmail.com>
2023-04-06 17:00:57 +0300

    model update   user model now doesnt contains first name and last name

--- models/user.js
+++ models/user.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  is_member: { type: Boolean, required: true },
-  is_admin: { type: Boolean, required: true },
+  is_member: { type: Boolean, default: false },
+  is_admin: { type: Boolean, default: false },
   messages: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -20,4 +18,4 @@ UserSchema.virtual("url").get(function () {
   return `/home/user/${this._id}`;
 });
 
-exports.module = new mongoose.model("User", UserSchema);
+module.exports = new mongoose.model("User", UserSchema);
