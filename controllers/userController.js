13f4a0051211b42bf63b944a1afa40731d7a172a
Mustafa <mustafamcm9@gmail.com>
2023-04-06 18:10:56 +0300

    user requests   user permission and create message dum functions( Mmber requests done)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,55 @@
 const User = require("../models/user");
 const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
 
+exports.get_become_member = (req, res, next) => {
+  res.render("member", { title: "Enter member passcode", user: req.user });
+};
+
+exports.post_become_member = [
+  body("password", "Member password is not correct!").equals(
+    process.env.MEMBER_PASSCODE
+  ),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors render the page again
+      res.render("member", {
+        title: "Enter member passcode",
+        user: req.user,
+        errors: errors.array(),
+      });
+    } else {
+      // Passcode is true, make user a member and redirect
+      async function updateField() {
+        try {
+          await User.findByIdAndUpdate(
+            req.user._id,
+            { is_member: true },
+            { new: true }
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+      updateField().then(() => res.redirect("/"));
+    }
+  },
+];
+
+exports.get_become_admin = (req, res, next) => {
+  res.send("become admin get");
+};
+
+exports.post_become_admin = (req, res, next) => {
+  res.send("become admin post");
+};
+
+exports.get_create_message = (req, res, next) => {
+  res.send("become create message get");
+};
+
+exports.post_create_message = (req, res, next) => {
+  res.send("become create message post");
+};
--- routes/home.js
+++ routes/home.js
@@ -14,10 +14,21 @@ router.get("/", (req, res) => {
 router.get("/sign-up", authController.get_sign_up);
 router.post("/sign-up", authController.post_sign_up);
 
-// LOG IN REQUESTS
+// LOG IN/OUT REQUESTS
 router.get("/log-in", authController.get_login);
 router.post("/log-in", authController.post_login);
 
 router.get("/log-out", authController.get_logout);
+
+// USER PERMISSON REQUESTS
+router.get("/become-member", userContoller.get_become_member);
+router.post("/become-member", userContoller.post_become_member);
+
+router.get("/become-admin", userContoller.get_become_admin);
+router.post("/become-admin", userContoller.post_become_admin);
+
+// USER MESSAGE REQUESTS
+router.get("/create_message", userContoller.get_create_message);
+router.post("/create_message", userContoller.post_create_message);
 
 module.exports = router;
