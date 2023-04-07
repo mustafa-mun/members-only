fe62fa7c182c4cd3f3108c26e18a3169aa702022
Mustafa <mustafamcm9@gmail.com>
2023-04-07 14:05:29 +0300

    create message and login check   This commit adds login check to all non auth requests and adds create message get, post requests

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,14 @@ const User = require("../models/user");
 const Message = require("../models/message");
 const { body, validationResult } = require("express-validator");
 
-exports.get_become_member = (req, res, next) => {
-  res.render("member", { title: "Enter member passcode", user: req.user });
+exports.get_become_member = (req, res) => {
+  // Check if user logged in
+  if (req.user) {
+    res.render("member", { title: "Enter member passcode", user: req.user });
+  } else {
+    // User is not logged in yet, redirect to log in page
+    res.redirect("/home/log-in");
+  }
 };
 
 exports.post_become_member = [
@@ -38,8 +44,14 @@ exports.post_become_member = [
   },
 ];
 
-exports.get_become_admin = (req, res, next) => {
-  res.render("admin", { title: "Enter admin passcode", user: req.user });
+exports.get_become_admin = (req, res) => {
+  // Check if user logged in
+  if (req.user) {
+    res.render("admin", { title: "Enter admin passcode", user: req.user });
+  } else {
+    // User is not logged in yet, redirect to log in page
+    res.redirect("/home/log-in");
+  }
 };
 
 exports.post_become_admin = [
@@ -85,10 +97,57 @@ exports.post_become_admin = [
   },
 ];
 
-exports.get_create_message = (req, res, next) => {
-  res.send("become create message get");
+exports.get_create_message = (req, res) => {
+  // Check if user is logged in
+  if (req.user) {
+    res.render("message", { title: "Create Message", user: req.user });
+  } else {
+    // User is not logged in yet, redirect to log in page
+    res.redirect("/home/log-in");
+  }
 };
 
-exports.post_create_message = (req, res, next) => {
-  res.send("become create message post");
-};
+exports.post_create_message = [
+  body("title", "Title needs to be minimum 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("message_body", "Message needs to be minimum 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors
+      res.render("message", {
+        title: "Create Message",
+        user: req.user,
+        errors: errors.array(),
+      });
+      return;
+    }
+    // Data is valid
+
+    // Create new message with the data
+    const message = new Message({
+      author: req.user._id,
+      title: req.body.title,
+      message_body: req.body.message_body,
+      timestamp: Date.now(),
+    });
+
+    try {
+      // Save message on database
+      const result = await message.save();
+      const user = await User.findOne({ _id: req.user._id });
+      // Push message to users messages field
+      user.messages.push(result);
+      await user.save();
+      res.redirect("/home");
+    } catch (error) {
+      return next(error);
+    }
+  },
+];
